b0f00357d7ed1a88a3a316bd28113caa0c7d52a4
your-name <abdellamahmoud98@gmail.com>
2024-12-09 11:52:37 -0500

    added 12.17: Feed App - Resource Provider & JwtService

---
+++ src/test/java/com/bptn/feedAppAutomation/jwt/JwtService.java
@@ -0,0 +1,37 @@
+package com.bptn.feedAppAutomation.jwt;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.auth0.jwt.JWT;
+import com.bptn.feedAppAutomation.provider.ResourceProvider;
+
+import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
+import java.util.Date;
+
+@Component
+public class JwtService {
+
+	@Autowired
+	ResourceProvider provider;
+
+
+	public String generateJwtToken(String username, long expiration) {
+
+	    return JWT.create()
+	             .withIssuer(this.provider.getJwtIssuer())
+	             .withAudience(this.provider.getJwtAudience())
+	             .withIssuedAt(new Date())
+	             .withSubject(username)
+	             .withExpiresAt(new Date(System.currentTimeMillis() + expiration))
+	             .sign(HMAC512(this.provider.getJwtSecret()));
+	}
+
+	public String getSubject(String token) {
+
+	    return JWT.require(HMAC512(this.provider.getJwtSecret()))
+	             .withIssuer(this.provider.getJwtIssuer())
+	             .build().verify(token).getSubject();
+	}
+
+}
