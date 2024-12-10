b9c4f0ecacf8a8373189aafbf448c65d61d501d3
your-name <abdellamahmoud98@gmail.com>
2024-12-10 15:53:39 -0500

    added 12.30: Feed App - Feed Page - Add a New Feed

--- src/test/java/com/bptn/feedAppAutomation/cucumberRunners/RunCucumberTest.java
+++ src/test/java/com/bptn/feedAppAutomation/cucumberRunners/RunCucumberTest.java
@@ -12,5 +12,6 @@ import static io.cucumber.junit.platform.engine.Constants.GLUE_PROPERTY_NAME;
 @SelectClasspathResource("com/bptn/feedAppAutomation/pageFeatures/registerPage.feature")
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "com.bptn.feedAppAutomation")
 @SelectClasspathResource("com/bptn/feedAppAutomation/pageFeatures/loginPage.feature")
+@SelectClasspathResource("com/bptn/feedAppAutomation/pageFeatures/feedPage.feature")
 public class RunCucumberTest {
 }
